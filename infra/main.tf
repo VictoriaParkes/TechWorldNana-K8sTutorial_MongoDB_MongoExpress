8b3be03a8717e139ebf43c62f4e140db8ab57783
VictoriaParkes <victoria.parkes@live.co.uk>
2026-02-25 14:42:40 +0000

    Add additional worker and controller nodes

--- infra/main.tf
+++ infra/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "k8_vpc" {
   cidr_block = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  
+
   tags = {
     Name = "${var.project_name}-vpc"
   }
@@ -213,10 +213,7 @@ resource "aws_lb" "load_balancer" {
   name               = "${var.project_name}-load-balancer"
   internal           = false
 
-  subnets = [
-    aws_subnet.k8_public_subnets["subnet1"].id,
-    aws_subnet.k8_public_subnets["subnet2"].id
-  ]
+  subnets = [for subnet in aws_subnet.k8_public_subnets : subnet.id]
 
   tags = {
     Name = "${var.project_name}-load-balancer"
--- infra/variables.tf
+++ infra/variables.tf
@@ -25,7 +25,7 @@ variable "vpc_cidr" {
 variable "public_subnet_cidrs" {
   description = "Public subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
   validation {
     condition     = length(var.public_subnet_cidrs) >= 2
@@ -36,7 +36,7 @@ variable "public_subnet_cidrs" {
 variable "private_subnet_cidrs" {
   description = "Private subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.10.0/24", "10.0.20.0/24"]
+  default     = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
 
   validation {
     condition     = length(var.private_subnet_cidrs) >= 1
