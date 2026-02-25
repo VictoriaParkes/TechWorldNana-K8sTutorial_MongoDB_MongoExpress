3b40a6d1257dc777a22555f8780920adb7ea9c80
VictoriaParkes <victoria.parkes@live.co.uk>
2026-02-25 10:16:14 +0000

    Add ip outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -13,3 +13,16 @@ output "caller_user" {
 output "K8_public_address" {
   value = aws_lb.load_balancer.dns_name
 }
+
+output "controller_private_ips" {
+  value = [for instance in aws_instance.K8s_controllers : instance.private_ip]
+}
+
+output "worker_private_ips" {
+  value = [for instance in aws_instance.K8s_workers : instance.private_ip]
+}
+
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
